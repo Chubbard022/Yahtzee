6d150835dc4dec55918d7001d47b4e3d06a6d9ae
Chubbard022 <chubbard022@gmail.com>
2020-03-29 10:26:17 -0700

    working more on dice logic

--- src/Components/DiceLogic.js
+++ src/Components/DiceLogic.js
@@ -5,16 +5,18 @@ import Dice from "./Dice"
 
 const DiceLogic = () =>{
     const [dice] = useState([1,2,3,4,5,6])
-    const [rolledDice,setRolledDice] = useState({diceOne:0,diceTwo:0,diceThree:0,diceFour:0,diceFive:0,diceSix:0})
+    const [rolledDice,setRolledDice] = useState({"diceOne":0,"diceTwo":0,"diceThree":0,"diceFour":0,"diceFive":0,"diceSix":0})
 
     function rollDice() {
 
         let obj = {}
 
         for (let [key, value] of Object.entries(rolledDice)) {
-            value = Math.floor(Math.random() * Math.floor(6))
-            console.log(key,value)
-            rolledDice[key] = value
+            let number = Math.floor(Math.random() * Math.floor(6))
+            console.log("key",typeof key)
+            console.log("value",number)
+            setRolledDice({...rollDice, key: number})
+            console.log(rolledDice)
         }
       }
     function setDice(){
@@ -26,7 +28,7 @@ const DiceLogic = () =>{
     return(
         <div>
         <button onClick={setDice}>ROLL DICE</button>
-        <Dice rolledDice={rolledDice}/>
+        <Dice rolledDice={rollDice}/>
         </div>
     )
 }
