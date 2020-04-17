985c3a7e8960b7dae87ce0cade38ca9eb74da8d7
curtis <chubbard022@gmail.com>
2020-04-17 10:13:13 -0700

    adding folder to later use to clean up gamelogic file

---
+++ src/Components/helperFunctions/ExtraYahtzee.js
@@ -0,0 +1,32 @@
+const [extraYahtzee,setExtraYahtzee] = useState({
+    gameOne: {
+      one: {display: "", Clicked: false},
+      two: {display: "", Clicked: false},
+      three: {display: "", Clicked: false}
+    },
+    gameTwo: {
+      one: {display: "", Clicked: false},
+      two: {display: "", Clicked: false},
+      three: {display: "", Clicked: false}
+    },
+    gameThree: {
+      one: {display: "", Clicked: false},
+      two: {display: "", Clicked: false},
+      three: {display: "", Clicked: false}
+    },
+    gameFour: {
+      one: {display: "", Clicked: false},
+      two: {display: "", Clicked: false},
+      three: {display: "", Clicked: false}
+    },
+    gameFive: {
+      one: {display: "", Clicked: false},
+      two: {display: "", Clicked: false},
+      three: {display: "", Clicked: false}
+    },
+    gameSix: {
+      one: {display: "", Clicked: false},
+      two: {display: "", Clicked: false},
+      three: {display: "", Clicked: false}
+    }
+  })
