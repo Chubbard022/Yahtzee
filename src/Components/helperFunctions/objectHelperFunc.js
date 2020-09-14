0318d14a1b6aa8513571bf60b8fada217a0c8c60
Chubbard022 <chubbard022@gmail.com>
2020-09-14 07:26:27 -0700

    starting refactoring process

---
+++ src/Components/helperFunctions/objectHelperFunc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    getValues,
+    objectAssigVal,
+    doubleObjAsigVal,
+    objectAssignKeyVal,
+    objectVal,
+    objectKey
+}
+
+function getValues(){
+    console.log("working")
+}
+
+//an object assign that goes over the values of an object.
+function objectAssigVal(){
+}
+
+//an object assign that goes through two objects
+function doubleObjAsigVal(obj1,obj2){
+
+}
+
+//an object assign that uses both keys and values
+function objectAssignKeyVal(){
+}
+
+//going over object values 
+function objectVal(){
+
+}
+
+//going over object keys
+function objectKey(){
+
+}
